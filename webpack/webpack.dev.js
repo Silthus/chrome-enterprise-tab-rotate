a9ba2f439a5f25cfd75fc94b76f4f8e56d55c68d
Michael Reichenbach <michael@reichenbach.in>
2019-12-27 09:28:14 +0100

    initial commit

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,12 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const ExtensionReloader = require('webpack-extension-reloader');
+
+module.exports = merge(common, {
+    devtool: 'inline-source-map',
+    mode: 'development',
+    plugins: [
+        ...common.plugins,
+        new ExtensionReloader()
+    ]
+});
