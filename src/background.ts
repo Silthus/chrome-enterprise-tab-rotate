eed4db123b86fa28bad2213a96d76bc639f6c6ba
Michael Reichenbach <michael@reichenbach.in>
2020-01-05 13:47:15 +0100

    cleanup

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,3 @@
-import { Config } from './models/config';
-import { CONFIG_UPDATED_MESSAGE } from './models/messages';
-import {
-  switchMap,
-  withLatestFrom,
-  map,
-  tap,
-  catchError
-} from 'rxjs/operators';
-import { interval, timer, of } from 'rxjs';
 import { TabRotator } from './models/tab-rotate';
 
 // will automatically start based on the json config
